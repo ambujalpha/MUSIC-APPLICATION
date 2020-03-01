1b4a579c5b7b161d7f9164c87eab858e9042abbf
ambujalpha <jainambuj8@gmail.com>
2020-03-01 12:57:12 +0530

    request_return

--- website/website/urls.py
+++ website/website/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('stocks/', views.StockList.as_view(), name='stocks'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
